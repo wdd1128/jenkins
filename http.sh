8fc7cb8c263ce1693c59da3609315ddb37393da2
wdd <wdd1128@naver.com>
2021-10-21 15:23:02 +0900

    http

--- http.sh
+++ http.sh
@@ -6,3 +6,7 @@ sudo wget https://downloads.apache.org/apr/apr-1.7.0.tar.gz
 sudo wget https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 sudo wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
 sudo wget https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
+sudo tar xzf apr-1.7.0.tar.gz
+sudo tar xzf apr-util-1.6.1.tar.gz
+sudo tar xzf pcre-8.45.tar.gz
+sudo tar xzf httpd-2.4.51.tar.gz
