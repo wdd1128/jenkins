ade3d22a012eef0f58943302bcbfc3e919f23a36
wdd <wdd1128@naver.com>
2021-10-21 16:06:39 +0900

    http

--- http.sh
+++ http.sh
@@ -3,10 +3,16 @@ sudo mkdir /web
 cd /web
 sudo yum install -y expat-devel gcc gcc-c++ wget
 
+if [ ! -e "/web/apr-1.7.0.tar.gz" ]
+then
+	echo "file not exsist"
 sudo wget https://downloads.apache.org/apr/apr-1.7.0.tar.gz
 sudo wget https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 sudo wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
 sudo wget https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
+else
+	echo "file exsist"
+fi
 
 if [ ! -d "/web/apr-1.7.0" ]
 then
