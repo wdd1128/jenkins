3d25f1bc4a265a4e5e1147f74e343c9c7c808860
wdd <wdd1128@naver.com>
2021-10-21 15:21:48 +0900

    http

--- http.sh
+++ http.sh
@@ -2,3 +2,7 @@
 sudo mkdir /web
 cd /web
 sudo yum install -y expat-devel gcc gcc-c++ wget
+sudo wget https://downloads.apache.org/apr/apr-1.7.0.tar.gz
+sudo wget https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
+sudo wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
+sudo wget https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
